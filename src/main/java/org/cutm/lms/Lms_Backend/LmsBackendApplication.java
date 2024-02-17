08d4aa2aae801812937f3c2c969c2184e4c40193
debasisnishank <nishankdebasis4@gmail.com>
2024-02-17 15:16:14 +0530

    repo init

---
+++ src/main/java/org/cutm/lms/Lms_Backend/LmsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cutm.lms.Lms_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cutm/lms/Lms_Backend/LmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cutm.lms.Lms_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
