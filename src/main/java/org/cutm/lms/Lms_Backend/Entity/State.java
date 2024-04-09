7c9ff3ce497c7ed7203165b0673b99c8d98a26d5
debasisnishank <nishankdebasis4@gmail.com>
2024-04-09 23:43:13 +0530

    state changes

--- src/main/java/org/cutm/lms/Lms_Backend/Entity/State.java
+++ src/main/java/org/cutm/lms/Lms_Backend/Entity/State.java
@@ -1,4 +1,5 @@
 package org.cutm.lms.Lms_Backend.Entity;
+import java.util.HashSet;
 import java.util.Set;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -19,6 +20,45 @@ public class State {
 	@JoinColumn(name = "adminId")
 	private Admin admin;
     @OneToMany(mappedBy = "state",cascade = CascadeType.ALL)
-    private Set<Campus> campuses;
+    private Set<Campus> campuses = new HashSet<Campus>();
+	public Long getStateId() {
+		return stateId;
+	}
+	public void setStateId(Long stateId) {
+		this.stateId = stateId;
+	}
+	public String getStateEmail() {
+		return stateEmail;
+	}
+	public void setStateEmail(String stateEmail) {
+		this.stateEmail = stateEmail;
+	}
+	public String getStatePasswd() {
+		return statePasswd;
+	}
+	public void setStatePasswd(String statePasswd) {
+		this.statePasswd = statePasswd;
+	}
+	public Admin getAdmin() {
+		return admin;
+	}
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+	public Set<Campus> getCampuses() {
+		return campuses;
+	}
+	public void setCampuses(Set<Campus> campuses) {
+		this.campuses = campuses;
+	}
+	public State() {}
+	public State(Long stateId, String stateEmail, String statePasswd, Admin admin, Set<Campus> campuses) {
 
+		this.stateId = stateId;
+		this.stateEmail = stateEmail;
+		this.statePasswd = statePasswd;
+		this.admin = admin;
+		this.campuses = campuses;
+	}
+   
 } 
