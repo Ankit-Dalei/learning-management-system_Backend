5957a7cc08c30f9dfa5a30133ad599f5a64aac04
debasisnishank <nishankdebasis4@gmail.com>
2024-04-15 15:47:51 +0530

    Role Repository created

--- src/main/java/org/cutm/lms/Lms_Backend/Repository/UserRoleRepository.java
+++ src/main/java/org/cutm/lms/Lms_Backend/Repository/UserRoleRepository.java
@@ -3,5 +3,5 @@ package org.cutm.lms.Lms_Backend.Repository;
 import org.cutm.lms.Lms_Backend.Entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepo extends JpaRepository<UserRole, String> {
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 }
