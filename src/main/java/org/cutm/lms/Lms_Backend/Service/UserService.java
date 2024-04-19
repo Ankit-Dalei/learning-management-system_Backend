f5f3b2cf6b5c4cec91650b83a38bac6700b3bd88
debasisnishank <nishankdebasis4@gmail.com>
2024-04-19 06:35:58 +0530

    new methods added

--- src/main/java/org/cutm/lms/Lms_Backend/Service/UserService.java
+++ src/main/java/org/cutm/lms/Lms_Backend/Service/UserService.java
@@ -1,12 +1,18 @@
 package org.cutm.lms.Lms_Backend.Service;
 
+import org.cutm.lms.Lms_Backend.Dto.LoginDto;
+import org.cutm.lms.Lms_Backend.Dto.RegisterDto;
 import org.cutm.lms.Lms_Backend.Dto.UserDto;
 import org.cutm.lms.Lms_Backend.Entity.User;
+import org.cutm.lms.Lms_Backend.Entity.UserRole;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserService {
     Optional<User> findByEmail(String email);
     UserDto findByEmailAndPassword(String email, String password);
     Boolean existByEmail(String email);
+    boolean createUserWithRoles(RegisterDto registerDto);
+    Set<String> login(LoginDto loginDto);
 }
